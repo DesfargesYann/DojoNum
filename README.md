3b05859d3203ef40be27c6dadc834cd66b8139e0
DesfargesYann <ydesfarges@ensc.fr>
2026-04-19 12:04:14 +0200

    changement URL dans service pour deploiement front

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # DojoNum
-Développement d’une application web permettant d’apprendre et de réviser les différentes connaissances du judo (quiz général, par grade, vocabulaire, arbitrage, etc.). Suivi de la progression d’un utilisateur (estimation de son grade, des connaissances qu’il lui reste à acquérir, évolution de ses résultats au fil du temps).
+Développement d’une application web permettant d’apprendre et de réviser les différentes connaissances du judo (quiz général, par grade, vocabulaire, arbitrage, etc...). Suivi de la progression d’un utilisateur (estimation de son grade, des connaissances qu’il lui reste à acquérir, évolution de ses résultats au fil du temps).
+
+Le but de ce projet était majoritairement de me former sur la réalisation d'une application web full stack.
--- react/src/components/ChoixNiveau.jsx
+++ react/src/components/ChoixNiveau.jsx
@@ -1,9 +1,14 @@
 export default function ChoixNiveau({setNiveauChoisi})
 {
     return(
-        <>
-        <button onClick={() => setNiveauChoisi(2)}> Ceinture blanche</button>
-        <button onClick={() => setNiveauChoisi(6)}> Ceinture marron</button>
-        </>
+        <div className="choix-niveau-container">
+            <button className="btn-ceinture btn-blanche" onClick={() => setNiveauChoisi(1)}>Ceinture blanche</button>
+            <button className="btn-ceinture btn-jaune" onClick={() => setNiveauChoisi(2)}>Ceinture jaune</button>
+            <button className="btn-ceinture btn-orange" onClick={() => setNiveauChoisi(3)}>Ceinture orange</button>
+            <button className="btn-ceinture btn-verte" onClick={() => setNiveauChoisi(4)}>Ceinture verte</button>
+            <button className="btn-ceinture btn-bleue" onClick={() => setNiveauChoisi(5)}>Ceinture bleue</button>
+            <button className="btn-ceinture btn-marron" onClick={() => setNiveauChoisi(6)}>Ceinture marron</button>
+            <button className="btn-ceinture btn-noire" onClick={() => setNiveauChoisi(7)}>Ceinture noire</button>
+        </div>
     )
 }
